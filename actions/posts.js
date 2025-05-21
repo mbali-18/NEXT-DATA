8263ff7eba76799cf75e4090b5bcf03ea1fe70c3
mbali-18 <faithfulservent18@gmail.com>
2025-05-21 16:42:47 +0200

    uploading & storing images

--- actions/posts.js
+++ actions/posts.js
@@ -1,6 +1,7 @@
 "use server";
 import { redirect } from "next/navigation";
 import { storePost } from "@/lib/posts";
+import { uploadImage } from "@/lib/cloudinary";
 export async function createPost(prevState, formData) {
   "use server";
   const title = formData.get("title");
@@ -24,8 +25,17 @@ export async function createPost(prevState, formData) {
   if (errors.length > 0) {
     return { errors };
   }
+  let imageUrl;
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    throw new Error(
+      "Image upload failed, post was not created. Please try again later."
+    );
+  }
+
   await storePost({
-    imageUrl: "",
+    imageUrl: imageUrl,
     title,
     content,
     userId: 1,
