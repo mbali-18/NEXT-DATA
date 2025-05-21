b4619eeae826c9a4392ae91a3ec5ebd123aaee3d
mbali-18 <faithfulservent18@gmail.com>
2025-05-21 19:06:40 +0200

    alternative ways of using, configuring & triggering server actions

--- actions/posts.js
+++ actions/posts.js
@@ -1,7 +1,10 @@
 "use server";
+
 import { redirect } from "next/navigation";
-import { storePost } from "@/lib/posts";
+
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+
 export async function createPost(prevState, formData) {
   "use server";
   const title = formData.get("title");
@@ -42,4 +45,8 @@ export async function createPost(prevState, formData) {
   });
 
   redirect("/feed");
+}
+
+export async function togglePostLikeStatus(postId) {
+  updatePostLikeStatus(postId, 2);
 }
